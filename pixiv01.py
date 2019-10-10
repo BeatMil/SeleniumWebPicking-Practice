c424d6950dc8f0d098cae517326d540799ccbd3b
BeatMil <anutorn@gmail.com>
2019-10-10 18:41:18 +0700

    Fail logining to Pixiv.net

--- pixiv01.py
+++ pixiv01.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+import time
+username = 'anutorn'
+password = 'Pobo88-7'
+
 
 def freezeQuit():
     user_choice = input('Please click ENTER button to close application')
@@ -19,9 +23,32 @@ browser = webdriver.Chrome()
 # browser.set_window_rect(100,100,800,800)
 # print(browser.get_window_rect())
 
+
+# Front page PIXIV
 browser.get("https://www.pixiv.net")
 elem = browser.find_element_by_link_text("Login")
 print(elem.text) # YASS It works!!
+elem.click()
+
+# Login page PIXIV
+login_field = browser.find_element_by_xpath("""//*[@id="LoginComponent"]/form/div[1]/div[1]/input""")
+password_field = browser.find_element_by_xpath("""//*[@id="LoginComponent"]/form/div[1]/div[2]/input""")
+
+login_field.send_keys(username)
+password_field.send_keys(password)
+password_field.submit()
+#reCapcha pop up
+print("sleeping...")
+time.sleep(3)
+print("slept for 3 second")
+# browser.find_element_by_class_name("recaptcha-checkbox-border").click()
+
+"""
+This is a comment?
+so I can't get past reCapcha.
+It too stonk.
+"""
+
 
 
 freezeQuit()
