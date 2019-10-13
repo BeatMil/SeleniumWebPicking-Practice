5aa9936d30ff4b36493fb1e7ab041b28fe01e0dc
BeatMil <anutorn@gmail.com>
2019-10-13 23:02:15 +0700

    Changes to be committed: 	new file:   animu-waifu-grabberV2.py It's working!! It can grab animu pics now but it's not compelete yet.

---
+++ hibikiV2/animu-waifu-grabberV2.py
@@ -0,0 +1,121 @@
+from selenium import webdriver
+from urllib import request
+import sys
+import time
+
+def freezeQuit():
+    user_choice = input('Please click ENTER button to close application')
+    if not user_choice:
+        print ("ABORTED")
+        browser.close()
+    else: 
+        print("don't type!!")
+        browser.close()
+
+def sleep(seconds):
+    print("sleeping for %d" % seconds)
+    time.sleep(seconds)
+
+
+animu_grill_name = "hibiki kancolle"
+# Google.com
+browser = webdriver.Chrome()
+browser.get("https://google.com")
+search_box = browser.find_element_by_name("q")
+# Search for the waifu
+search_box.send_keys(animu_grill_name)
+search_box.submit()
+# Go to images
+browser.find_element_by_link_text("Images").click()
+"""
+Open new tab with specific link!
+
+fish_web = "http://www.youtube.com"
+
+browser.execute_script('''window.open("%s","_blank");''' % fish_web)
+
+"""
+#############################################################################################
+# anime section
+# imgs = browser.find_elements_by_tag_name("img")
+
+# # TOP SECRET !! class = "irc_mi"
+# alt:  Image result for hibiki kancolle
+
+"""
+print("imgs type: %s" % type(imgs))
+
+print(("id: "), imgs[index].get_attribute("id"))
+print(("href: "), imgs[index].get_attribute("href"))
+"""
+# scroll down and wait then scroll again
+"""
+browser.execute_script("window.scrollTo(0, 40000)") 
+sleep(1)
+browser.execute_script("window.scrollTo(0, 40000)") 
+"""
+
+imgs = browser.find_elements_by_tag_name("img")
+src_amount = len(imgs)
+print("Amount of links: %d" % src_amount)
+
+# index = 30
+# print(("src: "), imgs[index].get_attribute("src"))
+# print(("alt: "), imgs[index].get_attribute("alt"))
+# if imgs[index].get_attribute("alt") == "Image result for hibiki kancolle":
+#         imgs[index].click()
+# else:
+#     print("not hibiki")
+
+big_images = []
+for i in range(len(imgs)):
+    print("i: %s" % i)
+    # print things defensively
+    try:
+        # print(("src: "), imgs[i].get_attribute("src"))
+        print(("alt: "), imgs[i].get_attribute("alt"))
+    except:
+        print("---get_attribute error---")
+    try:
+        if imgs[i].get_attribute("alt") == "Image result for hibiki kancolle":
+            try:
+                imgs[i].click()
+                big_images = browser.find_elements_by_class_name("irc_mi")
+                print("big_images range: %s" % len(big_images))
+            except:
+                print("---elem not clickable.---")
+        else:
+            print("not hibiki no click")
+    except:
+        print("---Error in if statement---")
+    if big_images and i%3 == 0:
+        for i2 in range(len(big_images)):
+            src = big_images[i2].get_attribute("src")
+            print("i2: %s" % i2)
+            print("src: %s" % src)
+            if src:
+                print("requesting!!!")
+                try:
+                    request.urlretrieve(src,("hibiki%s_%s.png" % (i,i2)))
+                except:
+                    print("---HTML Error---")
+    
+
+
+for image in big_images:
+    print(image.get_attribute("src"),"\n")
+
+
+
+
+# for i in range(src_amount):
+#     src = imgs[i].get_attribute("src")
+#     print("src: %s" % src)
+
+#     if src:
+#         print("%d: downloaded" % i)
+#         request.urlretrieve(src,"hibiki%d.png" % i)
+#     else:
+#         print("%d: no download" % i)
+
+freezeQuit()
