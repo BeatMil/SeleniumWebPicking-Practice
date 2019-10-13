59d589dc1ec26400eab3e4c89d248bdff59a82d9
BeatMil <anutorn@gmail.com>
2019-10-13 19:09:21 +0700

    Changes to be committed: 	modified:   animu-waifu-grabber.py
    Able to download thumbnail pictures of first page of google images

--- hibiki/animu-waifu-grabber.py
+++ hibiki/animu-waifu-grabber.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
 from urllib import request
 
+def freezeQuit():
+    user_choice = input('Please click ENTER button to close application')
+    if not user_choice:
+        print ("ABORTED")
+        browser.close()
+    else: 
+        print("don't type!!")
+        browser.close()
+
+
 animu_grill_name = "hibiki kancolle"
 # Google.com
 browser = webdriver.Chrome()
@@ -12,7 +22,32 @@ search_box.submit()
 # Go to images
 browser.find_element_by_link_text("Images").click()
 # Save the first pic
-img = browser.find_element_by_xpath("""//*[@id="USkkXF79ja6nJM:"]""")
-src = img.get_attribute('src')
+# img = browser.find_element_by_xpath("""//*[@id="USkkXF79ja6nJM:"]""")
+# src = img.get_attribute('src')
 
-request.urlretrieve(src, "hibiki.png")
+# request.urlretrieve(src, "hibiki.png")
+imgs = browser.find_elements_by_tag_name("img")
+
+# for img in imgs:
+#     i = 1
+#     src = img.get_attribute('src')
+#     print(src,"\n")
+#     request.urlretrieve(src, "hibiki.png")
+#     i += 1
+
+src_amount = len(imgs)
+print("Amount of links: %d" % src_amount)
+
+print("imgs type: %s" % type(imgs))
+
+for i in range(src_amount):
+    src = imgs[i].get_attribute("src")
+    print("src: %s" % src)
+
+    if src:
+        print("%d: downloaded" % i)
+        request.urlretrieve(src,"hibiki%d.png" % i)
+    else:
+        print("%d: no download" % i)
+
+freezeQuit()
