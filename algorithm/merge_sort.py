0d4fbc2e9e4723806df588591bc0c38c9a4c0806
BeatMil <anutorn@gmail.com>
2019-11-19 17:23:28 +0700

    merge sort done! (not correctly)

--- algorithm/merge_sort.py
+++ algorithm/merge_sort.py
@@ -1,11 +1,22 @@
 # merge_sort
 def merge_sort(arr):
-    for i in range(int(len(arr))):
-        if i < len(arr) - 1:
-            if arr[i] > arr[i+1]: 
-                box = arr[i]
-                arr[i] = arr[i+1]
-                arr[i+1] = box
+    for i in range(0,len(arr), 2):
+        if arr[i] > arr[i+1]: 
+            box = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = box
+    for i in range(3):
+        if arr[i] > arr[i+1]: 
+            box = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = box
+    for i in range(3):
+        if arr[i] > arr[i+1]: 
+            box = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = box
+
+        
 
 
 
