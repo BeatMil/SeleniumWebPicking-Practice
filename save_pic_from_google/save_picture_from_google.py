455351fb22c9253b3a7675c694fa7c3b1201fbcc
beatmil <anutorn@gmail.com>
2020-05-23 19:41:37 +0700

    gonna save by curl command

--- save_pic_from_google/save_picture_from_google.py
+++ save_pic_from_google/save_picture_from_google.py
@@ -1,9 +1,19 @@
-#from selenium import webdriver 
-#browser = webdriver.Firefox()
-#browser.get("http://www.youtube.com")
+# /usr/bin/python3
 
 import os
-os.system("echo yay")
-os.system("pwd")
+from selenium import webdriver
+keyword = 'nekopara vanilla'
+search_engine = 'https://www.google.com/imghp?hl=en'
+browser = webdriver.Firefox()
+browser.get(search_engine)
+#search_box = browser.find_elements_by_tag_name('input')
+#print(search_box)
+#print(len(search_box))
+search_box = browser.find_element_by_name('q')
+search_box.send_keys(keyword)
+search_box.submit()
+#os.system("echo yay")
+#os.system("pwd")
+
 
 
