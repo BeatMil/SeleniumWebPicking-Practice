66d560d0cf06f237431d1c441e1038b4666f7f0b
beatmil <anutorn@gmail.com>
2020-05-29 14:54:53 +0700

    upload to google v0.1

--- beat_google_photo/main.py
+++ beat_google_photo/main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from time import sleep
+import os
+import pyautogui as m
 
 google_signin_link = "https://accounts.google.com/signin/v2/identifier?" + \
         "passive=1209600&continue=https%3A%2F%2Fphotos.google.com%2F" + \
@@ -8,6 +10,7 @@ google_signin_link = "https://accounts.google.com/signin/v2/identifier?" + \
 gmail = "anutorn@gmail.com"
 password = "bob214*JIN*"
 browser = webdriver.Firefox()
+browser.maximize_window()
 browser.get(google_signin_link)
 browser.find_element_by_name("identifier").send_keys(gmail)
 browser.find_element_by_class_name("CwaK9").click()  # hit submit
@@ -15,11 +18,29 @@ sleep(2)
 browser.find_element_by_name("password").send_keys(password)
 browser.find_element_by_class_name("CwaK9").click()
 # we logged in!
-browser.find_element_by_xpath("""/html/body/div[1]/div/c-wiz/c-wiz/div/div[1]/div[2]/div[2]/div/div[2]/div/div[2]""").click()
+browser.find_element_by_xpath("""/html/body/div[1]/div/c-wiz/\
+        c-wiz/div/div[1]/div[2]/div[2]/div/div[2]/div/div[2]""").click()
 sleep(1)
 # css selector is the best!
-button = browser.find_element_by_css_selector("div.JPdR6b:nth-child(17) > div:nth-child(1) > div:nth-child(1) > span:nth-child(2) > div:nth-child(3) > div:nth-child(1)")
+the_css = "div.JPdR6b:nth-child(17) > div:nth-child(1) > " + \
+        "div:nth-child(1) > span:nth-child(2) > div:nth-child(3)" + \
+        " > div:nth-child(1)"
+button = browser.find_element_by_css_selector(the_css)
 button.click()
+sleep(1)
+m.moveTo(450, 510)
+m.click()
+m.move(200, 0)
+m.click()
+m.hotkey('ctrl', 'a')
+m.hotkey('alt', 'o')
+
+user_input = input("Delete pics? (Y/N)")
+if user_input.lower() == "y":
+    # delete the pics from ~/.save_to_google
+    os.system("rm /home/beatmil/.save_to_google/*")
+    os.system("y")
+
 # ActionChains(browser).click(button).perform()
 
 # span_tags = browser.find_elements_by_tag_name("span")
--- beat_google_photo/test_pyautogui.py
+++ beat_google_photo/test_pyautogui.py
@@ -1,5 +1,16 @@
 import pyautogui as m
 from time import sleep
+import os
 sleep(1)
-m.moveTo(100, 500)
-m.click()
+m.moveTo(450, 510)
+# m.click()
+m.move(200, 0)
+# m.click()
+# m.hotkey('ctrl', 'a')
+# m.hotkey('alt', 'o')
+
+# user_input = input("Delete pics? (Y/N)")
+# if user_input.lower() == "y":
+# # delete the pics from ~/.save_to_google
+# os.system("rm /home/beatmil/.save_to_google/*")
+# os.system("y")
